330445f4aa907b92bff5cec2bf7bac2183f113df
Jacob M. Slone <jslone1993@gmail.com>
2015-04-01 00:37:14 -0400

    sine

---
+++ Assets/Scripts/Equations/Sine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sine : Equation {
+	public float Amplitude;
+	public float Period;
+	public float Offset;
+
+	public override float eval (float x) {
+		return Amplitude * Mathf.Sin(2 * Mathf.PI * Period * x + Offset);
+	}
+}
