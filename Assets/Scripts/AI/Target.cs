98b44c3011543c3acaa5473f3baea168a2417126
Jacob M. Slone <jslone1993@gmail.com>
2015-04-01 01:20:33 -0400

    fate

--- Assets/Scripts/AI/Target.cs
+++ Assets/Scripts/AI/Target.cs
@@ -1,15 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Target : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public abstract class Target : MonoBehaviour {
 }
