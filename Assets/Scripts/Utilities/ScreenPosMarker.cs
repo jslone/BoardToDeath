9c4a1cc7bfdb2bf7c4ee8ea1838a1c2943497f00
Jacob M. Slone <jslone1993@gmail.com>
2015-04-02 17:13:30 -0400

    debuging art

---
+++ Assets/Scripts/Utilities/ScreenPosMarker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenPosMarker : MonoBehaviour {
+	public Vector2 ScreenPosition;
+	// Use this for initialization
+	void Start () {
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector2(ScreenPosition.x * Screen.width, ScreenPosition.y * Screen.height));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+#if UNITY_EDITOR
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector2(ScreenPosition.x * Screen.width, ScreenPosition.y * Screen.height));
+#endif
+	}
+}
