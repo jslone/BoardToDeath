a3c12cfc2be29b35c6661f1294c424fab6b30c9b
Jacob M. Slone <jslone1993@gmail.com>
2015-04-24 06:35:26 -0400

    monsters if threads not cut

--- Assets/Scripts/Thread/Thread.cs
+++ Assets/Scripts/Thread/Thread.cs
@@ -2,7 +2,14 @@
 using System.Collections;
 
 public class Thread : MonoBehaviour {
+	public Logistic positionEquation;
+	public TimedEquations position;
 	public TimedEquations patience;
+	public SpriteRenderer renderer;
+	public RPGCharacter potentialMonster;
+
+	private bool monsterSpawned = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,10 +17,17 @@ public class Thread : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(!monsterSpawned && positionEquation.Height - position.value < 0.01f) {
+			Souls.AddMonster(potentialMonster);
+			monsterSpawned = true;
+		}
+		renderer.color = Color.Lerp(Color.white, Color.red, 2*position.value/positionEquation.Height - 1.0f);
 	}
 
 	public virtual void Cut() {
+		// kill living monster
+		potentialMonster.Health.x = 0;
+
 		// Spawn soul
 		GameObject soul = SoulSpawner.UseSpawner();
 
