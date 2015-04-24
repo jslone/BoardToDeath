7ecd8156851eb9d81956ca314655ec7387775a7e
Jacob M. Slone <jslone1993@gmail.com>
2015-04-24 03:44:08 -0400

    working upgrade menu

---
+++ Assets/Scripts/UI/UIUpgradeMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIUpgradeMover : MonoBehaviour {
+	public RectTransform rTrans;
+	public float speed;
+	public int loc;
+	public float[] positions;
+
+	// Use this for initialization
+	void Start () {
+		rTrans = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = rTrans.localPosition;
+		pos.x = Mathf.Lerp(pos.x,positions[loc],speed * Time.deltaTime);
+		rTrans.localPosition = pos;
+	}
+
+	public void Move(int dir) {
+		loc = Mathf.Clamp(loc+dir,0,positions.Length-1);
+	}
+}
---
+++ Assets/Scripts/UI/UIUpgradeNav.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIUpgradeNav : MonoBehaviour {
+	public Text text;
+	public UIUpgradeMover upgrades;
+	public string[] names;
+	public int dir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = names[upgrades.loc];
+	}
+
+	public void Navigate() {
+		upgrades.Move(dir);
+	}
+}
