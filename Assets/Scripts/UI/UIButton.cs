a0471ccb98bdab3fb8be42c7dc906554c2a65513
Diana Zhu <dianaz@andrew.cmu.edu>
2015-04-26 02:07:57 -0400

    added title menu scene

---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ButtonAction {
+	PLAY,
+	TUTORIAL,
+	QUIT
+}
+
+public class UIButton : MonoBehaviour {
+	public ButtonAction action;
+	
+	public void OnClick() {
+		switch(action) {
+			case ButtonAction.PLAY:
+				Application.LoadLevel("game");	// game scene
+				break;
+			case ButtonAction.TUTORIAL:
+				Application.LoadLevel("tutorial1");	// title scene
+				break;
+/* 			case ButtonAction.CREDITS:
+				Application.LoadLevel("credits");	// credits scene
+				break; 
+*/
+			case ButtonAction.QUIT:
+				Application.Quit();
+				break;
+		}
+	}
+}
