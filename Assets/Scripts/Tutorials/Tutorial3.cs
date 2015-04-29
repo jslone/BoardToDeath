db28431f870f52324f6586f50008f570a846af44
Ivan Wang <icw@cmu.edu>
2015-04-29 00:35:08 -0400

    tutorial 3 updates

--- Assets/Scripts/Tutorials/Tutorial3.cs
+++ Assets/Scripts/Tutorials/Tutorial3.cs
@@ -5,45 +5,64 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Tutorial3 : Tutorial {
-	public Button MenuButton;
 	public List<GameObject> Threads;
 	public List<GameObject> BoardingLines;
+	public Button MenuButton;
+	public Button MenuLeftButton;
+	public Button MenuRightButton;
 
 	GameObject currentMessage;
 	int progress = 0;
 
 	// Continue in tutorial
 	protected override void ProceedTutorial() {
-		Vector3 pos = new Vector3(4f, 2.75f, 0); // position of text message
+		Vector3 pos = new Vector3(0, -3f, 0); // position of text message
 		switch (phase++) {
 			case 0:
 				GameTime.paused = true;
 				ToggleInteraction(false);
+				pos = new Vector3(4f, 2.75f, 0);
 				CreateMessage(pos, "Unfortunately, due to budget cuts, we can only afford to give you one boat.");
 				break;
 
 			case 1:
+				pos = new Vector3(4f, 2.75f, 0);
 				CreateMessage(pos, "However, once you've ferried enough souls, you can use them to purchase upgrades!");
 				break;
 
 			case 2:
 				pos = new Vector3(5f, 3.25f, 0);
 				GameTime.paused = false;
-				ToggleMenuButton(true);
+				ToggleButton(MenuButton, true);
 				currentMessage = CreateMessage(pos, "Click the Menu button to pause the game and view possible upgrades.", -1);
 				break;
 
 			// opened menu
 			case 3:
-				pos = new Vector3(0f, -3f, 0);
-				ToggleMenuButton(false);
+				ToggleButton(MenuButton, false);
 				Destroy(currentMessage);
-				CreateMessage(pos, "Here, you can upgrade Atropos' walking speed, the cut window of threads, and even buy an upgrade to slow time.");
+				CreateMessage(pos, "Here, you can upgrade Atropos' walking speed, the cut margin of threads, and even buy an upgrade to slow time.");
 				break;
 
 			case 4:
-				pos = new Vector3(0f, -3f, 0);
-				// TODO: scroll left
+				MenuLeftButton.GetComponent<UIUpgradeNav>().Navigate(); // scroll left
+				CreateMessage(pos, "In this menu, you can buy and upgrade the speed and capacity of different boats.");
+				break;
+
+			case 5:
+				Souls.souls = 15;
+				currentMessage = CreateMessage(pos, "Here are 15 souls. Purchase a speed boat with it!", -1);
+				break;
+
+			case 6:
+				Destroy(currentMessage);
+				ToggleButton(MenuButton, true);
+				pos = new Vector3(5f, 3.25f, 0);
+				currentMessage = CreateMessage(pos, "Click on Menu to unpause the game and see your new ship.", -1);
+				break;
+
+			case 7:
+				Destroy(currentMessage);
 				break;
 
 			default:
@@ -53,8 +72,11 @@ public class Tutorial3 : Tutorial {
 	}
 
 	public override void UpdateProgress(int value) {
-		// opened menu
-		if (value == 0 && phase == 3) {
+		if (value == 0 && (phase == 3 || phase == 7)) {
+			// opened or closed menu
+			ProceedTutorial();
+		} else if (value == 1 && phase == 6) {
+			// purchased boat
 			ProceedTutorial();
 		}
 	}
@@ -62,15 +84,21 @@ public class Tutorial3 : Tutorial {
 	void ToggleInteraction(bool on) {
 		foreach (GameObject thread in Threads) {
 			thread.GetComponent<BoxCollider2D>().enabled = on;
+			thread.GetComponent<RandomSpawner>().enabled = on;
 		}
 		foreach (GameObject line in BoardingLines) {
 			line.GetComponent<BoxCollider2D>().enabled = on;
 		}
-		ToggleMenuButton(on);
+		ToggleButton(MenuButton, on);
+		ToggleButton(MenuLeftButton, on);
+		ToggleButton(MenuRightButton, on);
 	}
 
-	void ToggleMenuButton(bool on) {
-		MenuButton.interactable = on;
-		MenuButton.GetComponent<EventTrigger>().enabled = on;
+	void ToggleButton(Button button, bool on) {
+		button.interactable = on;
+		EventTrigger trigger = button.GetComponent<EventTrigger>();
+		if (trigger) {
+			trigger.enabled = on;
+		}
 	}
 }
