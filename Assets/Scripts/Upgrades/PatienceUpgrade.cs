12d725cf95bcd7fb08414f2b106714df0b456af2
Jacob M. Slone <jslone1993@gmail.com>
2015-04-30 01:12:14 -0400

    thread width

---
+++ Assets/Scripts/Upgrades/PatienceUpgrade.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatienceUpgrade : Upgradeable {
+	public float[] patienceScales;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Thread.PatienceScale = patienceScales[UpgradeLevel];
+	}
+}
