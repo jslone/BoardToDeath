c468dd020a53075fb375d48685418f7550453361
Ivan Wang <icw@cmu.edu>
2016-01-12 00:36:43 -0500

    update to v5.3.1; fix scaling of cracks

--- Assets/Scripts/UI/UIStats.cs
+++ Assets/Scripts/UI/UIStats.cs
@@ -23,21 +23,21 @@ public class UIStats : MonoBehaviour {
 		switch(type) {
 			case Type.SOULS:
 				text.text = "Current:\t" + Souls.souls + "\n"
-						+	"Spent:\t\t" + (Souls.soulsTotal - Souls.souls) + "\n"
-						+	"Total:\t\t" + Souls.soulsTotal;
+						+	"Spent:\t" + (Souls.soulsTotal - Souls.souls) + "\n"
+						+	"Total:\t" + Souls.soulsTotal;
 				break;
 			case Type.HEROES:
 				text.text = "Current:\t" + Souls.heroes + "\n"
-						+	"Killed:\t\t" + (Souls.heroesTotal - Souls.heroes) + "\n"
-						+	"Total:\t\t" + Souls.heroesTotal;
+						+	"Killed:  \t" + (Souls.heroesTotal - Souls.heroes) + "\n"
+						+	"Total:\t" + Souls.heroesTotal;
 				break;
 			case Type.MONSTERS:
 				text.text = "Current:\t" + Souls.monsters + "\n"
-						+	"Killed:\t\t" + (Souls.monstersTotal - Souls.monsters) + "\n"
-						+	"Total:\t\t" + Souls.monstersTotal;
+						+	"Killed:  \t" + (Souls.monstersTotal - Souls.monsters) + "\n"
+						+	"Total:\t" + Souls.monstersTotal;
 				break;
 			case Type.DISASTERS:
-				text.text = "Total:\t\t" + NaturalDisaster.count;
+				text.text = "Total:\t" + NaturalDisaster.count;
 				break;
 		}
 	}
